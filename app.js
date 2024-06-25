a3641124f56cb8188e4fe91d63b7fbb0e342c890
Lisa Bridges <lbridges@staff.wvnet.edu>
2024-06-25 14:14:40 -0400

    adding event listener

--- app.js
+++ app.js
@@ -59,3 +59,17 @@ function displayEmployees(employeeData) {
         overlay.classList.remove("hidden");
         modalContainer.innerHTML = modalHTML;
     }
+    gridContainer.addEventListener('click', e => {
+        // make sure the click is not on the gridContainer itself
+        if (e.target !== gridContainer) {
+        // select the card element based on its proximity to actual element
+        clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+        displayModal(index);
+        }
+        });
+
+        modalClose.addEventListener('click', () => {
+            overlay.classList.add("hidden");
+            });
